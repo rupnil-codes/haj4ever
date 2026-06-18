9bd84759df8ec7bc659d7df08ff48fe4b1a75614
Flux3tor <tusharsinha199@gmail.com>
2026-06-18 21:57:17 +0530

    made landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <p>Hello, World!</p>
-    </>
-  )
-}
+    <div className="app">
+      <img src="/flag.png" alt="" className="flag" />
 
-export default App
+      <main className="hero">
+        <div className="title-wrap">
+          <div className="new-drop">{`{NEW DROP!!!}`}</div>
+
+          <h1 className="site-name">
+            HACK CLUB HAJ4EVER
+          </h1>
+        </div>
+
+        <p className="tagline">
+          ship anything shark/ocean related get a blahaj/smolhaj!
+        </p>
+
+        <a
+          className="btn-submit"
+          href="https://haj4ever.fillout.com/submit"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/submit.png" alt="submit" />
+        </a>
+
+        <section className="cards">
+          <div className="card card-blahaj">
+            <img src="/blahaj.png" alt="blahaj" />
+          </div>
+
+          <div className="card card-meme">
+            <img src="/meme.png" alt="meme" />
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
